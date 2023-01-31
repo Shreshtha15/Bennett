17d8e705ea23953b7a105f11dd901b7629cce0a3
Shreshtha <shreshtha1509@gmail.com>
2023-01-31 11:53:59 +0530

    cmt_msg_022

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,28 @@
-# Your Python Code...
+class Shape:
+    def __init__(self, color):
+        self.color = color
 
-#...................
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, color, side_length):
+        super().__init__(color)
+        self.side_length = side_length
+
+    def area(self):
+        return self.side_length**2
+
+class Circle(Shape):
+    def __init__(self, color, radius):
+        super().__init__(color)
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+square = Square("red", 5)
+print("Square area:", square.area(), "color:", square.color)
+
+circle = Circle("blue", 3)
+print("Circle area:", circle.area(), "color:", circle.color)
